ceadc36c7143f1f0e1ef2f87344f1ab9dfd26dea
Stanislas Marion <stanislas.marion@gmail.com>
2011-11-24 22:14:00 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PapillonnoceurFirst::Application.config.secret_token = 'e2ed42c8035195b3757279d231534431dd2a5789353b9ff0d229cc0d5c1cf4c924fea4a4f08ed1ce9a74fc715ff549455e1130dc10b37e52261efab310b87c99'

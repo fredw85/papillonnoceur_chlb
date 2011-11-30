b0800c5fb7055a663958136f5781d2e12e7cd783
Stanislas Marion <stanislas.marion@gmail.com>
2011-11-30 03:40:36 +0100

    various fixes and password protection

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,26 +2,33 @@
 
 class PagesController < ApplicationController
   def accueil
+    @title = "Infos pratiques"
     @messe = "Messe à 15h à l'Eglise de Brives-La-Gaillarde"
     @reception = "Reception à 17h au château de Chambord"
   end
 
   def presentation
+    @title = "Présentation"
   end
 
   def trajet
+    @title = "Trajet"
   end
 
   def covoiturage
+    @title = "Covoiturage"
   end
 
   def logement
+    @title = "Logement"
   end
 
   def liste
+    @title = "Liste"
   end
 
   def photos
+    @title = "Photos"
   end
 
 end
